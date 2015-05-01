b6ee7ebcce568357fb816d94f510aa5263933d6f
Lennard Timm <lennard@71grad.de>
2015-05-01 13:41:17 +0200

    Iterate on raindrops

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -5,8 +5,6 @@ class Fixnum
 end
 
 class Raindrops
-  PRIME_FACTORS = [3, 5, 7]
-
   SOUNDS_FOR_FACTORS = {
     '3' => 'Pling',
     '5' => 'Plang',
@@ -14,7 +12,9 @@ class Raindrops
   }
   
   def self.convert(number)
-    PRIME_FACTORS.each_with_object(sound = '') do |factor|
+    factors = SOUNDS_FOR_FACTORS.keys.map(&:to_i)
+    
+    factors.each_with_object(sound = '') do |factor|
       sound << SOUNDS_FOR_FACTORS[factor.to_s] if factor.prime_factor_of?(number)
     end
     
